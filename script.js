2c266bf97cff9c52a4070ad58144d854d9568776
Mahmudul Hasan <162016075+humaneediedofanxiety@users.noreply.github.com>
2024-05-08 23:30:27 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let count = document.getElementsByClassName('count')[0];
 let sleep = document.getElementById('sleep');
 let h4 = document.querySelectorAll('h4');
 let li = document.querySelectorAll('.tasks > li');
+let cons = document.getElementById('cons');
 
 let countIndex = localStorage.getItem('countIn')
 count.innerHTML = countIndex;
@@ -12,19 +13,22 @@ if (countIndex === null) {
 }
 
 if (countIndex <= 7) {
-    li[0].innerHTML = 'get up before 10 AM'
+    li[0].innerHTML = 'get up before 12 AM'
     li[1].innerHTML = 'read 3 pages of a book'
     sleep.innerHTML = 'To Sleep Before 3:00 AM'
     h4[0].innerHTML = 'stage I <span class="active"><<</span>'
+    cons.innerHTML = "consumption=after_seven_pm" 
 } else if (countIndex <= 14) {
-    li[1].innerHTML = 'read 5 pages of a book'
+    li[1].innerHTML = 'read 3 pages of a book'
     li[4].innerHTML = 'deep work for 90 minutes'
     sleep.innerHTML = 'To Sleep Before 2:30 AM'
     h4[1].innerHTML = 'stage I <span class="active"><<</span>'
 } else if (countIndex <= 21) {
+    li[0].innerHTML = 'get up before 10 AM'
     li[3].innerHTML = 'exercise for 25 minutes'
     sleep.innerHTML = 'To Sleep Before 2:00 AM'
     h4[2].innerHTML = 'stage I <span class="active"><<</span>'
+    cons.innerHTML = "consumption=after_nine_pm" 
 } else if (countIndex <= 28) {
     sleep.innerHTML = 'To Sleep Before 1:00 AM'
     li[4].innerHTML = 'deep work for 120 minutes'
@@ -35,6 +39,7 @@ if (countIndex <= 7) {
     li[4].innerHTML = 'deep work for 180 minutes'
     sleep.innerHTML = 'To Sleep Before 12:00 AM'
     h4[4].innerHTML = 'stage I <span class="active"><<</span>'
+    cons.innerHTML = "consumption=after_ten_pm" 
 }
 
 for (let i = 0; i < taskItems.length; i++) {
